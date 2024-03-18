4c451e43668c35a8c674f6a96c11508b5abe6ec1
Jake Browning <jacobbrowning90@gmail.com>
2024-03-18 13:46:25 +0000

    Change port to enable simultaneous backend and frontend testing

--- app.js
+++ app.js
@@ -39,4 +39,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// var port = process.env.PORT || 3000;
+
+// app.listen(port, function () {
+//   console.log('Example app listening on port ' + port + '!');
+// });
+
 module.exports = app;
