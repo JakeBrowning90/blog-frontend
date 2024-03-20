6dd4ca0d6d30b6d979570dd926f0d41fc97c3b30
Jake Browning <jacobbrowning90@gmail.com>
2024-03-20 19:57:28 +0000

    Add reader_create form

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,27 @@ router.get('/dashboard', async function(req, res, next) {
   res.render('index', { title: 'Blog: Author Dashboard', postList: postList});
 });
 
+router.get('/sign-up', async function(req, res, next) {
+  res.render('reader_create', { title: 'Sign-Up Form' });
+});
+
+router.post('/sign-up', async function(req, res, next) {
+  const response = await fetch('http://localhost:3000/readers/', {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      password: req.body.password,
+      is_admin: false
+    })
+  })
+  // return response.json();
+  res.redirect('/')
+});
+
 module.exports = router;
