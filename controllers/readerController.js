fd05d845555f00c1d7057266126435ccaa77119e
Jake Browning <jacobbrowning90@gmail.com>
2024-04-18 17:19:26 +0100

    Change login response logic to if/else

--- controllers/readerController.js
+++ controllers/readerController.js
@@ -29,21 +29,22 @@ exports.log_in_post = asyncHandler(async (req, res, next) => {
    
   // return response.json();
   console.log(response.status);
+
   if (response.status == 401) {
     res.render("reader_login", { 
       title: "Reader Log-In",
       errorMessage: "Incorrect email/password"
     });
+  } else {
+    const loginResponse = await response.json();
+    console.log(loginResponse);
+    // Save user info to localStorage
+    localStorage.setItem('email', loginResponse.email);
+    localStorage.setItem('full_name', loginResponse.full_name);
+    localStorage.setItem('id', loginResponse.id);
+    // console.log(localStorage.getItem('email'));
+    res.redirect('/');
   }
-
-  const loginResponse = await response.json();
-  console.log(loginResponse);
-  // Save user info to localStorage
-  localStorage.setItem('email', loginResponse.email);
-  localStorage.setItem('full_name', loginResponse.full_name);
-  localStorage.setItem('id', loginResponse.id);
-  // console.log(localStorage.getItem('email'));
-  res.redirect('/');
 });
 
 exports.log_out = asyncHandler(async (req, res, next) => {
